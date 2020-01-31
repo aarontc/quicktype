b297adc4b1a50d2b73a3fdebc48ceb871e89626b
Maxim Indykov <indmaksim@gmail.com>
2020-01-31 19:25:39 +0300

    Fix Docker

--- test/languages.ts
+++ test/languages.ts
@@ -316,8 +316,8 @@ export const RubyLanguage: Language = {
     "top-level-enum.schema",
   ],
   skipMiscJSON: false,
-  rendererOptions: {},
-  quickTestRendererOptions: [],
+  rendererOptions: { "legacy-dry": "true" },
+  quickTestRendererOptions: [{ "legacy-dry": "true" }],
   sourceFiles: ["src/language/ruby/index.ts"],
 };
 
